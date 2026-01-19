1b3bbe03ed436cca44f8b93960e4955acd6e828d
Dhruvan Teja <dhruvanteja.talapaneni@gmail.com>
2026-01-19 14:07:19 -0600

    updated code

--- SpikePrime/Python/LeftBase/Task3.py
+++ SpikePrime/Python/LeftBase/Task3.py
@@ -10,52 +10,55 @@ import math
 WHEEL_DIAMETER_INCH = 2.2
 WHEEL_CIRCUMFERENCE = math.pi * WHEEL_DIAMETER_INCH
 
+
 def inches_to_degrees(distance_inch):
     rotations = distance_inch / WHEEL_CIRCUMFERENCE
     return int(rotations * 360)
 
-# ------------------------
-# Drive functions (A & E)
-# ------------------------
-async def drive_distance(distance_in_inch, velocity=360):
-    degrees = inches_to_degrees(abs(distance_in_inch))
-    steering = 0# straight
+def turn_degrees(angle_degree):
+    return int(angle_degree * 2.5)
 
-    speed = velocity if distance_in_inch > 0 else -velocity
+# Wheel configurations
+async def move_forward(distance_in_inches, velocity = 360):
+    await move_wheels(distance_in_inches, velocity)
+
+async def move_backward(distance_in_inches, velocity = 360):
+    await move_wheels(distance_in_inches, -velocity)
+
+async def move_wheels(distance_in_inches, velocity = 360):
+    degrees = inches_to_degrees(abs(distance_in_inches))
+    steering = 0
     await motor_pair.move_for_degrees(
         motor_pair.PAIR_1,
         degrees,
         steering,
-        velocity=speed
+        velocity = velocity
     )
 
-async def attachment_right(angle_deg, velocity=200):
-    TURN_FACTOR = 2.5
-    degrees = int(abs(angle_deg) * TURN_FACTOR)
+async def turn_right_wheel(angle_degree, velocity):
+    await turn_wheel(port.E, -angle_degree, velocity)
 
-    speed = velocity if angle_deg > 0 else -velocity
-    await motor.run_for_degrees(port.B, degrees, speed)
+async def turn_left_wheel(angle_degree, velocity):
+    await turn_wheel(port.A, angle_degree, velocity)
 
-async def attachment_left(angle_deg, velocity=200):
-    TURN_FACTOR = 2.5
-    degrees = int(abs(angle_deg) * TURN_FACTOR)
+async def turn_wheel(port, angle_degree, velocity):
+    await motor.run_for_degrees(port, turn_degrees(angle_degree), velocity)
 
-    speed = velocity if angle_deg > 0 else -velocity
-    await motor.run_for_degrees(port.F, degrees, speed)
+# Attachment configurations
+async def move_right_attachment(angle_degree, velocity, move_up = True):
+    await move_attachment(port.B, angle_degree, velocity, move_up)
 
-async def right_turn(angle_deg, velocity=200):
-    TURN_FACTOR = -2.5
-    degrees = int(abs(angle_deg) * TURN_FACTOR)
+async def move_left_attachment(angle_degree, velocity, move_up = True):
+    await move_attachment(port.F, angle_degree, velocity, move_up)
 
-    speed = velocity if angle_deg > 0 else -velocity
-    await motor.run_for_degrees(port.E, degrees, speed)
+async def move_attachment(port, angle_degree, velocity, move_up = True):
+    if move_up:
+        angle_degree = angle_degree
+    else:
+        angle_degree = -angle_degree
 
-async def left_turn(angle_deg, velocity=200):
-    TURN_FACTOR = 2.5
-    degrees = int(abs(angle_deg) * TURN_FACTOR)
+    await motor.run_for_degrees(port, turn_degrees(angle_degree), velocity)
 
-    speed = velocity if angle_deg > 0 else -velocity
-    await motor.run_for_degrees(port.A, degrees, speed)
 
 # ------------------------
 # Main program
@@ -63,12 +66,15 @@ async def left_turn(angle_deg, velocity=200):
 async def main():
     # Pair drive motors ONLY
     motor_pair.pair(motor_pair.PAIR_1, port.E, port.A)
+
     
-    await drive_distance(26, 5000)
-    await left_turn(35, 5000)
-    await drive_distance(4,5000)
-    await left_turn(42,5000)
-    await drive_distance(-12,5000)
-    await right_turn(115,5000)
-    await drive_distance(-25, 5000)
+
+    await move_forward(26, 5000)
+    await turn_left_wheel(35, 5000)
+    await move_forward(4,5000)
+    await turn_left_wheel(42,5000)
+    await move_backward(12,5000)
+    await turn_right_wheel(115,5000)
+    await move_backward(25, 5000)
 runloop.run(main())
+
